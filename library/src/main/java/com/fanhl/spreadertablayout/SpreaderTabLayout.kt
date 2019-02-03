dd95c4853add976a599cb25bc0f0b48848cb0364
dyguests@qq.com <dyguest0315>
2019-02-03 18:55:15 +0800

    spreader init

--- library/src/main/java/com/fanhl/spreadertablayout/SpreaderTabLayout.kt
+++ library/src/main/java/com/fanhl/spreadertablayout/SpreaderTabLayout.kt
@@ -1,7 +1,24 @@
 package com.fanhl.spreadertablayout
 
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.util.AttributeSet
+import android.widget.HorizontalScrollView
+import androidx.viewpager.widget.ViewPager.DecorView
+
 /**
  * @author fanhl
  */
-class SpreaderTabLayout {
+@DecorView
+class SpreaderTabLayout @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : HorizontalScrollView(context, attrs, defStyleAttr) {
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+        canvas?.drawColor(Color.RED)
+    }
 }
--- sample/src/main/java/com/fanhl/spreadertablayout/sample/MainActivity.kt
+++ sample/src/main/java/com/fanhl/spreadertablayout/sample/MainActivity.kt
@@ -1,12 +1,14 @@
 package com.fanhl.spreadertablayout.sample
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.tabs.TabLayout
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        TabLayout::class
     }
 }
