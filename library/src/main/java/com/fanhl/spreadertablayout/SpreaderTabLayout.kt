7ad10ab27d9f3ed63d5e6cec593a99677f73970d
dyguests@qq.com <dyguest0315>
2019-02-04 18:05:40 +0800

    DefaultTabsWrapperProvider

--- library/src/main/java/com/fanhl/spreadertablayout/SpreaderTabLayout.kt
+++ library/src/main/java/com/fanhl/spreadertablayout/SpreaderTabLayout.kt
@@ -3,8 +3,10 @@ package com.fanhl.spreadertablayout
 import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Color
+import android.graphics.Paint
 import android.util.AttributeSet
 import android.widget.HorizontalScrollView
+import androidx.annotation.Dimension
 import androidx.viewpager.widget.ViewPager.DecorView
 
 /**
@@ -16,9 +18,64 @@ class SpreaderTabLayout @JvmOverloads constructor(
     attrs: AttributeSet? = null,
     defStyleAttr: Int = 0
 ) : HorizontalScrollView(context, attrs, defStyleAttr) {
+    var tabsWrapperProvider: ITabsWrapperProvider = DefaultTabsWrapperProvider()
+
+    /** FIXME 调试用*/
+    var tabCount = 2
+
+    var position = 0f
+
+    init {
+        val theme = context.theme
+
+        val a = theme.obtainStyledAttributes(attrs, R.styleable.SpreaderTabLayout, defStyleAttr, R.style.Widget_Spreader_Tab_Layout)
+
+        a.recycle()
+
+        tabsWrapperProvider.tabCount = tabCount
+    }
 
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
         canvas?.drawColor(Color.RED)
+
+        tabsWrapperProvider.draw(canvas ?: return, position)
+    }
+
+    @Dimension(unit = 1)
+    internal fun dpToPx(@Dimension(unit = 0) dps: Int): Int {
+        return Math.round(this.resources.displayMetrics.density * dps.toFloat())
+    }
+
+    companion object {
+        @Dimension(unit = 0)
+        private const val DEFAULT_HEIGHT = 48
+    }
+
+    /**
+     * Tabs的包裹绘制提供者
+     */
+    interface ITabsWrapperProvider {
+        /**FIXME*/
+        var tabCount: Int
+
+        fun draw(canvas: Canvas, position: Float)
+    }
+
+    /**
+     * 默认Tabs容器
+     */
+    class DefaultTabsWrapperProvider : ITabsWrapperProvider {
+        override var tabCount: Int = 0
+
+        private val paint = Paint()
+
+        init {
+            paint.color = Color.BLUE
+        }
+
+        override fun draw(canvas: Canvas, position: Float) {
+            canvas.drawRect(0F, 0F, 100F, canvas.height.toFloat(), paint)
+        }
     }
 }
--- sample/src/main/java/com/fanhl/spreadertablayout/sample/MainActivity.kt
+++ sample/src/main/java/com/fanhl/spreadertablayout/sample/MainActivity.kt
@@ -3,6 +3,7 @@ package com.fanhl.spreadertablayout.sample
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.tabs.TabLayout
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,5 +11,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         TabLayout::class
+
+        tab_layout.tabCount
     }
 }
