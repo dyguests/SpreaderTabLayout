1a1b960c946693acb904857562e8fb30fd3ad628
fanhl <fanhonglin@klicen.com>
2019-03-01 10:10:39 +0800

    works

--- library/src/main/java/com/fanhl/spreadertablayout/SpreaderTabLayout.kt
+++ library/src/main/java/com/fanhl/spreadertablayout/SpreaderTabLayout.kt
@@ -1,4 +1,28 @@
 package com.fanhl.spreadertablayout
 
-class SpreaderTabLayout {
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.util.AttributeSet
+import android.view.ViewGroup
+
+class SpreaderTabLayout @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : ViewGroup(context, attrs, defStyleAttr) {
+    private val paint by lazy { Paint(Paint.ANTI_ALIAS_FLAG) }
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+    }
+
+    override fun onLayout(changed: Boolean, l: Int, t: Int, r: Int, b: Int) {
+
+    }
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas ?: return)
+        canvas.drawCircle(0f, 0f, 100f, paint)
+    }
 }
